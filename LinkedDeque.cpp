03c96388b3c40cacaa353dfe50a9dffa7c9cff2f
martaMartiUB <mmartima175@alumnes.ub.edu>
2018-03-29 13:04:46 +0200

    Inicialització de la classe LinkedDeque

---
+++ LinkedDeque.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   LinkedDeque.cpp
+ * Author: Marta
+ * 
+ * Created on 29 de marzo de 2018, 12:50
+ */
+
+#include "LinkedDeque.h"
+
+LinkedDeque::LinkedDeque() {
+}
+
+LinkedDeque::LinkedDeque(const LinkedDeque<Element>& deque) {
+}
+
+LinkedDeque::~LinkedDeque() {
+}
+
