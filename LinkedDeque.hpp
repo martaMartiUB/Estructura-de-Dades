1cf15ed6de9c564402629d9c0e2cdfdb0c0a1518
martaMartiUB <mmartima175@alumnes.ub.edu>
2018-04-14 17:02:17 +0200

    Canvis

--- LinkedDeque.hpp
+++ LinkedDeque.hpp
@@ -1,3 +1,31 @@
+#ifndef LINKEDDEQUE_H
+#define LINKEDDEQUE_H
+
+#include "Node.hpp"
+
+template <class Element>
+class LinkedDeque {
+ public:
+ LinkedDeque();
+ ~LinkedDeque();
+
+LinkedDeque(const LinkedDeque<Element>& deque);
+
+bool isEmpty()const;
+void insertFront(const Element & element);
+void insertRear(const Element & element);
+void deleteFront();
+void deleteRear();
+void print();
+const Element& getFront()const;
+const Element& getRear()const;
+
+private:
+Node<Element> *_front;
+Node<Element> *_rear;
+int num_elements;
+};
+
 
 template <class Element>
 LinkedDeque<Element>::LinkedDeque() {
@@ -55,4 +83,8 @@ void LinkedDeque<Element>::deleteFront() {
 template <class Element>
 LinkedDeque<Element>::~LinkedDeque() {
 }
+
+
+
+#endif /* LINKEDDEQUE_H */
 
