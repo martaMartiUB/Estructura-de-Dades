1afa670767c8ab940969ed60eceda31a32b4784a
martaMartiUB <mmartima175@alumnes.ub.edu>
2018-04-15 17:31:11 +0200

    crida metodes

--- main_2.cpp
+++ main_2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int menu() {
 	int option;
 	vector <string> arr_options = { "Llegir un fitxer amb les entrades de la cua d’impressió", "Eliminar una impressió pel davant", "Eliminar una impressió pel final",
-		"Eliminar element pel final", "Inserir n entrades d’impressió des de teclat (0 per finalizar)", "Imprimir la cua d’impressió", "Sortir" };
+		"Inserir n entrades d’impressió des de teclat (0 per finalizar)", "Imprimir la cua d’impressió", "Sortir" };
 
 	for (size_t i = 0; i < arr_options.size(); i++) {
 		cout << i + 1 << ". " << arr_options[i] << endl;
@@ -26,7 +26,10 @@ int menu() {
 
 
 int main() {
+
+	LinkedDeque <int> linDeq;
 	int option;
+
 	do {
 		cout << "hola que vols fer?" << endl;
 		option = menu();
@@ -39,13 +42,15 @@ int main() {
 		}
 		case (2):
 		{
-			//
+			linDeq.deleteFront();
+			cout << "Element esborrat pel davant";
 
 			break;
 		}
 		case (3):
 		{
-			//
+			linDeq.deleteRear();
+			cout << "Element esborrat pel darrere";
 			break;
 		}
 		case (4):
@@ -55,14 +60,10 @@ int main() {
 		}
 		case (5):
 		{
-			//
+			linDeq.print();
 			break;
 		}
 		case (6):
-		{
-
-		}
-		case (7):
 		{
 			cout << "Fins despres!" << endl;
 			option = 0;
